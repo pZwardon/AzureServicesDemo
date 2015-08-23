25590305c016b066b848085561597baa56b6b842
Krzysztof Szabelski <kszabelski@gmail.com>
2015-08-23 20:40:41 +0200

    Generated MVC project

---
+++ AzureServicesDemo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AzureServicesDemo.Startup))]
+namespace AzureServicesDemo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
